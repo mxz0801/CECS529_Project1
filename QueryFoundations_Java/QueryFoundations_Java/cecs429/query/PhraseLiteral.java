c36e3ae6174e3ab2e8bf1227c00c46451c11cfb8
Martin <mxz0801@users.noreply.github.com>
2020-09-24 10:15:31 -0700

    init

---
+++ QueryFoundations_Java/QueryFoundations_Java/cecs429/query/PhraseLiteral.java
@@ -0,0 +1,35 @@
+package cecs429.query;
+
+import cecs429.index.Index;
+import cecs429.index.Posting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Represents a phrase literal consisting of one or more terms that must occur in sequence.
+ */
+public class PhraseLiteral implements Query {
+	// The list of individual terms in the phrase.
+	private List<Query> mChildren = new ArrayList<>();
+	
+	/**
+	 * Constructs a PhraseLiteral with the given individual phrase terms.
+	 */
+	public PhraseLiteral(Iterable<Query> children) {
+		mChildren.addAll(terms);
+	}
+	
+	@Override
+	public List<Posting> getPostings(Index index) {
+		return null;
+		// TODO: program this method. Retrieve the postings for the individual terms in the phrase,
+		// and positional merge them together.
+	}
+	
+	@Override
+	public String toString() {
+		return "\"" + String.join(" ", mTerms) + "\"";
+	}
+}
