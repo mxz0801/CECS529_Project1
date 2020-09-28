02d010a29ac6c67fefc6be03957421a417098baf
Martin <mxz0801@users.noreply.github.com>
2020-09-28 00:55:08 -0700

    simplify reading

--- src/main/java/cecs429/index/PositionalInvertedIndex.java
+++ src/main/java/cecs429/index/PositionalInvertedIndex.java
@@ -6,7 +6,7 @@ import java.util.*;
  */
 
 public class PositionalInvertedIndex implements Index {
-	private Map<String,ArrayList<Posting>> mIndex= new HashMap<String, ArrayList<Posting>>();
+	private Map<String,List<Posting>> mIndex= new HashMap<String, List<Posting>>();
 	private List<String> mVocabulary = new ArrayList<String>();
 	
 	/**
