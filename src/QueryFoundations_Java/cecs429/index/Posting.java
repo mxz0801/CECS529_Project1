92148dd6c84acb9a828c4df228b22b413d237422
Martin <mxz0801@users.noreply.github.com>
2020-09-27 22:03:10 -0700

    PositionalIndex

---
+++ src/QueryFoundations_Java/cecs429/index/Posting.java
@@ -0,0 +1,23 @@
+package cecs429.index;
+
+import java.util.ArrayList;
+
+/**
+ * A Posting encapulates a document ID associated with a search query component.
+ */
+public class Posting {
+	private int mDocumentId;
+	private ArrayList<Integer> mPosition;
+	public Posting(int documentId, ArrayList<Integer> position) {
+		mDocumentId = documentId;
+		mPosition = position;
+	}
+	
+	public int getDocumentId() {
+		return mDocumentId;
+	}
+	
+	public ArrayList<Integer> getPosition(){
+		return mPosition;
+	}
+}
