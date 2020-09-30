d020e0c9a23e62c6dfe80ddfe0533c4b5a3205e8
Martin <mxz0801@users.noreply.github.com>
2020-09-30 00:06:25 -0700

    implement Porter2 Stemmer using SnowballProgram

--- src/main/java/cecs429/text/ImprovedTokenProcessor.java
+++ src/main/java/cecs429/text/ImprovedTokenProcessor.java
@@ -1,10 +1,14 @@
 package cecs429.text;
 
+import org.tartarus.snowball.SnowballStemmer;
+
 import java.util.*;
 
 public class ImprovedTokenProcessor implements TokenProcessor {
 	@Override
-	public List<String> processToken(String token) {
+	public List<String> processToken(String token) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+		Class stemClass = Class.forName("org.tartarus.snowball.ext.englishStemmer");
+		SnowballStemmer stemmer = (SnowballStemmer) stemClass.newInstance();
 		List<String> processedToken = new ArrayList<>();
 		if(token.contains("-")){
 			processedToken.add(token.replaceAll("\\-",""));
@@ -16,17 +20,21 @@ public class ImprovedTokenProcessor implements TokenProcessor {
 			processedToken.add(token);
 		}
 		for(int i=0;i<processedToken.size();i++){
-			String tempProcess = processedToken.get(i).replaceAll("\\W", "");
-			tempProcess = tempProcess.replaceAll("^[^a-zA-Z0-9\\s]+|[^a-zA-Z0-9\\s]+$", "").toLowerCase();
-			tempProcess = tempProcess.replaceAll("['\"]", "");
-
+			String tempProcess = processedToken.get(i).replaceAll("\\W", "")
+					.replaceAll("^[^a-zA-Z0-9\\s]+|[^a-zA-Z0-9\\s]+$", "").replaceAll("['\"]", "").toLowerCase();
+			//tempProcess = tempProcess.replaceAll("^[^a-zA-Z0-9\\s]+|[^a-zA-Z0-9\\s]+$", "").toLowerCase();
+			//tempProcess = tempProcess.replaceAll("['\"]", "");
+			stemmer.setCurrent(tempProcess);
+			stemmer.stem();
 			processedToken.remove(i);
-			processedToken.add(i,tempProcess);
+			processedToken.add(i,stemmer.getCurrent());
 		}
 		//all non-alphanumeric characters from the beginning and end of the token
 
 		//processedToken.add(processedToken.replaceAll("['\"]", ""));
 		//remove apostropes and quotation marks
+
+
 		return processedToken;
 
 
